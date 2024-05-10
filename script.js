1d87f3c6cd268f64e2823f0a7ea015322bcdfe95
Ayush Tiwari <ayushtiwari@live.in>
2024-05-10 10:15:18 +0530

    update regex

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
-checkMessageButton.addEventListener("click", () => {});
+
+const helpRegex = /please help|assist me/i;
+const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
+const freeRegex = /free money/i;
+
+const denyList = [helpRegex, dollarRegex, freeRegex];
+
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
+
+checkMessageButton.addEventListener("click", () => {
+  if (messageInput.trim() === "") {
+    alert("Please enter a message");
+    return;
+  }
+  result.textContent = isSpam(messageInput.value)
+    ? "Oh no! This looks like a spam message."
+    : "This message does not seem to contain any spam.";
+  messageInput.value = "";
+});
